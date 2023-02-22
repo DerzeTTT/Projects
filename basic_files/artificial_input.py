12eee6ea8adb6ae18c76224127a4b294d4f61a2a
DerzeTTTT <u13_davdro_lbn@technischools.com>
2023-02-22 10:55:26 +0100

    Changes

--- basic_files/artificial_input.py
+++ basic_files/artificial_input.py
@@ -5,6 +5,8 @@ class Artificial_Input:
         self.Holding = [];
         self.Reading = 0;
 
+        self.Production = False;
+
     def add_input(self, new_input, split_lines=True):
 
         Appending = None;
@@ -23,16 +25,21 @@ class Artificial_Input:
 
         self.Holding += Appending;
 
-        print(self.Holding);
-
     def read(self):
 
-        Returning = self.Holding[self.Reading];
-        self.Reading += 1;
+        if not self.Production:
 
-        return Returning;
+            Returning = self.Holding[self.Reading];
+            self.Reading += 1;
+
+            return Returning;
+        
+        else:
+
+            return input();
 
 Custom_Input = Artificial_Input();
 
-Custom_Input.add_input('''
-''')
+Custom_Input.Production = False;
+
+Custom_Input.add_input('''''')
--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -5,6 +5,8 @@ class Artificial_Input:
         self.Holding = [];
         self.Reading = 0;
 
+        self.Production = False;
+
     def add_input(self, new_input, split_lines=True):
 
         Appending = None;
@@ -23,16 +25,32 @@ class Artificial_Input:
 
         self.Holding += Appending;
 
-        print(self.Holding);
-
     def read(self):
 
-        Returning = self.Holding[self.Reading];
-        self.Reading += 1;
+        if not self.Production:
 
-        return Returning;
+            Returning = self.Holding[self.Reading];
+            self.Reading += 1;
+
+            return Returning;
+        
+        else:
+
+            return input();
 
 Custom_Input = Artificial_Input();
 
-Custom_Input.add_input('''
+Custom_Input.Production = False;
+
+Custom_Input.add_input('''4
+Jan Kowalski 5 6 5 4 6 5
+Anna Nowak 3 4 2 5 3 4
+Piotr Testowski 1 1 3 1 5
+Julia Soska 5 5 5 5 5 5 5 5
 ''')
+
+Reading = int(Custom_Input.read());
+
+for i in range(Reading):
+
+    print("Lol", Custom_Input.read());
